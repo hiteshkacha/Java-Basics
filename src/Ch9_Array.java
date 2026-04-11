cfd2d17d1cb882dd79544cc5cc32910fa90e328e
hiteshkacha <hiteshdevac@gmail.com>
2026-04-11 23:46:37 +0530

    Added Ch9 Array

---
+++ src/Ch9_Array.java
@@ -0,0 +1,9 @@
+public class Ch9_Array {
+    public static void main(String[] args){
+        int array[] = {10,20,30};
+        array[2] = 15;
+        for(int i=0;i<array.length;i++){
+            System.out.println(array[i]);
+        }
+    }
+}
