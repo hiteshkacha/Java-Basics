13ffa5dee2ea9c5874b5f27cabd9fa4490cf62a6
hiteshkacha <hiteshdevac@gmail.com>
2026-04-11 23:48:51 +0530

    Added String Array

--- src/Ch9_Array.java
+++ src/Ch9_Array.java
@@ -1,9 +1,13 @@
 public class Ch9_Array {
     public static void main(String[] args){
         int array[] = {10,20,30};
+        String string[] = {"Arrya1", "Array2","Array3"};
         array[2] = 15;
         for(int i=0;i<array.length;i++){
             System.out.println(array[i]);
+        }
+        for(int i=0;i<string.length;i++){
+            System.out.println(string[i]);
         }
     }
 }
