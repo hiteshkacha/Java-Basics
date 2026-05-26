244c9cc81d8a6f7ae0dea86da3e0a610f05e0c0e
hiteshkacha <hiteshdevac@gmail.com>
2026-05-26 14:35:41 +0530

    String Module : String Diffrences

---
+++ src/string/Strings.java
@@ -0,0 +1,26 @@
+package string;
+
+public class Strings {
+    static void main(String[] args) {
+
+        String name = "Hitesh";
+        String sameName = "Hitesh";
+        String newName = new String("Hitesh");
+
+        if(name == sameName){
+            System.out.println("Both names are same.");
+        }
+
+        if(name == newName){
+            System.out.println("Both names are same.");
+        }else{
+            System.out.println("Both names are diffrent.");
+        }
+
+        // Equals Method
+        if(name.equals(newName)){
+            System.out.println("Both name have same values");
+        }
+    }
+
+}
