b00165d41eedcb47d3706f6f3b67738de85312e0
hiteshkacha <hiteshdevac@gmail.com>
2026-06-02 17:12:21 +0530

    OOPS 3 : Use of Static Keyword

---
+++ src/oops3/statickeyword/CustomMathClass.java
@@ -0,0 +1,7 @@
+package oops3.statickeyword;
+
+public class CustomMathClass {
+    public static double getSum(int num1, int num2){
+        return num1+num2;
+    }
+}
---
+++ src/oops3/statickeyword/StaticKeywordExample.java
@@ -0,0 +1,7 @@
+package oops3.statickeyword;
+
+public class StaticKeywordExample {
+    static void main(String[] args) {
+        System.out.println(CustomMathClass.getSum(10,20));
+    }
+}
