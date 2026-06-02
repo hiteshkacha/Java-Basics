94253fe6f7f9e1dd5b58e031506056b421cafb97
hiteshkacha <hiteshdevac@gmail.com>
2026-06-02 08:49:46 +0530

    OOPS 2 : Inheritance

---
+++ src/oops2/Inheritance.java
@@ -0,0 +1,9 @@
+package oops2;
+
+public class Inheritance extends InheritanceSubClass{
+    static void main(String[] args) {
+        Inheritance in = new Inheritance();
+        in.name = "Hitesh";
+        in.start();
+    }
+}
---
+++ src/oops2/InheritanceSubClass.java
@@ -0,0 +1,9 @@
+package oops2;
+
+public class InheritanceSubClass {
+    String name;
+
+    void start(){
+        System.out.println(name+" Starts Running...");
+    }
+}
